b99ecf0ac2044c47e5c0e222f41b7f0db5bace41
Juan Revelo <revelojuan88@gmail.com>
2023-07-02 22:05:19 -0500

    Actualización index.js server.js

--- fruver-be/index.js
+++ fruver-be/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import { userRoutes } from './routes/users.js';
 import { productRoutes } from './routes/products.js';
+import { ordersRoutes } from './routes/orders.js';
 export const app = express();
 
 app.use(cors());
@@ -11,3 +12,4 @@ app.use(express.urlencoded({extended: true, limit: '20mb'})); // Se aumenta el l
 app.use(express.json({limit: '20mb'}));// Se aumenta el límite a 20mb
 app.use('/users', userRoutes);
 app.use('/products', productRoutes);
+app.use('/orders', ordersRoutes);
--- fruver-be/server.js
+++ fruver-be/server.js
@@ -4,8 +4,8 @@ import { app } from './index.js';
 // Test a base de datos
 const testDb = async() => {
     try {
-        await sequelize.sync();
-        //await sequelize.authenticate();
+        //await sequelize.sync();
+        await sequelize.authenticate();
         console.log('Conexión realizada con éxito');
         // Correr el servicio por puerto 3000
         app.listen(process.env.PORT, () =>{
